d8b44b80441a9a551afc812820e86dd781c2303b
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-20 00:14:18 +0100

    Add eat fish from a river method

--- bear.rb
+++ bear.rb
@@ -4,7 +4,15 @@ class Bear
     def initialize(name, type)
         @name = name
         @type = type
+        @stomach = []
     end
 
+    def count_fish_in_stomach()
+        return @stomach.count()
+    end
+
+    def eat_fish_from_a_river(river)
+        @stomach.push(river.supply_fish())
+    end
 
 end
--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -1,6 +1,8 @@
 require('minitest/autorun')
 require('minitest/reporters')
 require_relative('../bear')
+require_relative('../river')
+require_relative('../fish')
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
@@ -8,6 +10,11 @@ class BearTest < MiniTest::Test
 
     def setup()
         @bear = Bear.new("Yogi", "Grizzly")
+        @river = River.new("Amazon")
+        @fish1 = Fish.new("Dory")
+        @fish2 = Fish.new("Nemo")
+        @river.add_fish(@fish1)
+        @river.add_fish(@fish2)
     end
 
     def test_bear_name_and_type()
@@ -15,5 +22,10 @@ class BearTest < MiniTest::Test
         assert_equal("Grizzly", @bear.type())
     end
 
+    def test_eat_fish_from_a_river()
+        @bear.eat_fish_from_a_river(@river)
+        assert_equal(1, @river.count_fish())
+        assert_equal(1, @bear.count_fish_in_stomach())
+    end
 
 end

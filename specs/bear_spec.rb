480533924f2b0113934f8e74ed686ea504dec95b
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-19 23:47:54 +0100

    Get bear name and type

--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -6,5 +6,14 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class BearTest < MiniTest::Test
 
+    def setup()
+        @bear = Bear.new("Yogi", "Grizzly")
+    end
+
+    def test_bear_name_and_type()
+        assert_equal("Yogi", @bear.name())
+        assert_equal("Grizzly", @bear.type())
+    end
+
 
 end
